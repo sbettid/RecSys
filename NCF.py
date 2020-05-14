09ce0eec0100b0b3ff914e371b38bf9ec9288b2f
sbettid <davide.sbetti@gmail.com>
2020-05-14 15:33:47 +0200

    Add files via upload

--- NCF.py
+++ NCF.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 import warnings
 import pydot
 
-warnings.filterwarnings('ignore')
 from sklearn.model_selection import train_test_split
 import keras
 from keras.optimizers import Adam
 from keras.utils.vis_utils import model_to_dot
 from sklearn.metrics import mean_absolute_error
 from keras.utils.vis_utils import model_to_dot
-keras.utils.vis_utils.pydot = pydot
+
 
 class NeuralCollaborativeFiltering:
 
 	def __init__(self, n_users, n_movies, n_latent_factors, learning_rate, n_epochs):
+		keras.utils.vis_utils.pydot = pydot
 		self.n_users = n_users
 		self.n_movies = n_movies
 		self.n_latent_factors = n_latent_factors
